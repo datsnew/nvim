16bebbe0fe1667443e630a2ce098ec0514a5f7ef
datsnew <peder.wenstad@norconsult.com>
2024-04-10 10:21:38 +0200

    added c-hjkl pane navigation

--- lua/datsnew/options.lua
+++ lua/datsnew/options.lua
@@ -55,6 +55,11 @@ vim.o.termguicolors = true
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>', { silent = true })
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>', { silent = true })
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>', { silent = true })
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>', { silent = true })
+
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
