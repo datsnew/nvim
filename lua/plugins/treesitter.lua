6ce41eb1d0fd00a9fb5b6011837244785032e163
Peder Wenstad <peder.wenstad@norconsult.com>
2024-05-13 15:27:26 +0200

    tresitter fix

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,11 +4,11 @@ return {
 	config = function()
 		local configs =	require('nvim-treesitter.configs')
 		configs.setup({
-		  ensure_installed = {"lua"},
+		  ensure_installed = {"lua", "bicep"},
 		  sync_install = false,
 		  auto_install = true,
 		  highlight = { enable = true },
 		  indent = { enable = true }
 		})
-		end		
+    end
 }
